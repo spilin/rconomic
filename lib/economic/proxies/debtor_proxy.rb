e4fc39c601f85c3d591363eeb8efbe03f7e35d89
Alex Krasinsky <lyoshakr@gmail.com>
2013-11-01 00:37:53 +0200

    Added Debtor_Delete support

--- lib/economic/proxies/debtor_proxy.rb
+++ lib/economic/proxies/debtor_proxy.rb
@@ -3,6 +3,8 @@ require "economic/proxies/actions/find_by_ci_number"
 require "economic/proxies/actions/find_by_handle_with_number"
 require "economic/proxies/actions/find_by_number"
 require "economic/proxies/actions/find_by_telephone_and_fax_number"
+require "economic/proxies/actions/delete"
+
 
 module Economic
   class DebtorProxy < EntityProxy
